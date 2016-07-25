a906a36101603d2c11bd7638008a311eb127194d
Craig England <craigengland21@gmail.com>
2016-07-25 09:35:44 +0100

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+$('.slide-container').slick({
+  slidesToShow: 1,
+  dots: true,
+  prevArrow: false,
+  nextArrow: false
+});
+
+if ($(window).innerWidth > 1024) {
+  $('.member-image').matchHeight();
+  $('.member-content').matchHeight();
+}
