d7a75cd6aed2d969324fba243d5e5444018adbb8
Craig England <craig.england@hookson.com>
2016-09-05 13:33:45 +0100

    Revert "Initial commit"
    This reverts commit 8391842d5719d2dfe67c2dd97fdb1a0df0f6e9a6.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,8 @@
 $('.slide-container').slick({
   slidesToShow: 1,
   dots: true,
-  prevArrow: '<img src="assets/img/arrow-left-hero.png" class="hero-arrow" />',
-  nextArrow: '<img src="assets/img/arrow-right-hero.png" class="hero-arrow" />'
-});
-
-$('.artist-slide-container').slick({
-  dots: true,
-  slidesToShow: 1,
-  prevArrow: '<img src="assets/img/arrow-left-second-slider.png" class="slider-arrow" />',
-  nextArrow: '<img src="assets/img/arrow-right-second-slider.png" class="slider-arrow" />'
-
+  prevArrow: false,
+  nextArrow: false
 });
 
 if ($(window).innerWidth > 1024) {
